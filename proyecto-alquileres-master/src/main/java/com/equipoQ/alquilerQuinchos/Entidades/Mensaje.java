dcaf8a4aa5012764e6793b1f430ddc0fdf9b3540
“Mrcx3” <marcelo.m95@hotmail.com>
2023-11-29 21:46:28 -0300

    modificacion de codigo

--- proyecto-alquileres-master/src/main/java/com/equipoQ/alquilerQuinchos/Entidades/Mensaje.java
+++ proyecto-alquileres-master/src/main/java/com/equipoQ/alquilerQuinchos/Entidades/Mensaje.java
@@ -1,6 +1,8 @@
 package com.equipoQ.alquilerQuinchos.Entidades;
 import javax.persistence.*;
+import lombok.Data;
 @Entity
+@Data
 public class Mensaje {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,46 +17,5 @@ public class Mensaje {
     @JoinColumn(name = "receptor_id")
     private Usuario receptor;
 
-   public Mensaje() {
-   }
-
-   public Mensaje(Long id, String texto, Usuario emisor, Usuario receptor) {
-      this.id = id;
-      this.texto = texto;
-      this.emisor = emisor;
-      this.receptor = receptor;
-   }
-
-   public Long getId() {
-      return id;
-   }
-
-   public void setId(Long id) {
-      this.id = id;
-   }
-
-   public String getTexto() {
-      return texto;
-   }
-
-   public void setTexto(String texto) {
-      this.texto = texto;
-   }
-
-   public Usuario getEmisor() {
-      return emisor;
-   }
-
-   public void setEmisor(Usuario emisor) {
-      this.emisor = emisor;
-   }
-
-   public Usuario getReceptor() {
-      return receptor;
-   }
-
-   public void setReceptor(Usuario receptor) {
-      this.receptor = receptor;
-   }
-   
+  
 }
